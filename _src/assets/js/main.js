7aae85620c1d931df9a9d264619721e13e6282e9
Marta Prieto <martaprietoluque@gmail.com>
2019-08-08 16:27:09 +0200

    add fav list

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -5,8 +5,9 @@ console.log('>> Ready :)');
 const title = document.querySelector('.input__name');
 const button = document.querySelector('.btn');
 const result = document.querySelector('.list');
+const favorites = document.querySelector('.serie');
 const url = 'http://api.tvmaze.com/search/shows?q=';
-
+const favs = [];
 
 function getList() {
   const nameSerie = title.value;
@@ -19,16 +20,16 @@ function getList() {
       for (const item of data) {
         if (item.show.image === null) {
           seriesResult += `
-          <li>
+          <li class="list__title-serie" data-id="${item.show.id}" data-name="${item.show.name}" data-img="">
             <h2 class="title__line"> ${item.show.name}</h2>
             <img class="img__serie" src="https://via.placeholder.com/210x295/f2f2ff/?text=TV" alt="imagen de:${item.show.name}">
           </li>
         `;
         } else {
           seriesResult += `
-          <li>
+          <li class="list__title-serie">
             <h2 class="title__line"> ${item.show.name}</h2>
-            <img class="img__serie" src="${item.show.image.medium}" alt="imagen de:${item.show.name}">
+            <img class="img__serie" src="${item.show.image.medium}" alt="imagen de:${item.show.name} ">
           </li>
         `;
         }
@@ -36,31 +37,45 @@ function getList() {
 
       }
       result.innerHTML += seriesResult;
+
+      const series = document.querySelectorAll('.list__title-serie');
+
+      for (let i = 0; i < series.length; i++) {
+        series[i].addEventListener('click', getFavs);
+      }
+      console.log('favorito-1');
     });
+
+}
+
+function getFavs(event) {
+  const item = event.currentTarget;
+  const id = item.getAttribute('data-id');
+  const name = item.getAttribute('data-name');
+  const img = item.getAttribute('data-img');
+  let seriesResultFav = '';
+  item.classList.toggle('serie-fav');
+  if (item.classList.contains('serie-fav')) {
+    if (favs.includes(name) === false) {
+      favs.push(name); {
+
+        seriesResultFav +=
+          `<li class="list__title-serie">
+        <h2 class="title__line"> ${name}</h2>
+        <img class="img__serie" src="${img}" alt="imagen de:${name} ">
+      </li>`;
+        console.log('hola');
+
+      }
+    }
+  } else {
+    const best = favs.indexOf(name);
+    if (best > -1) {
+      favs.splice(best, 1);
+    }
+  }
+  console.log('prueba-favorito-2');
+ favorites.innerHTML += seriesResultFav;
 }
 
 button.addEventListener('click', getList);
-
-
-
-
-/*   let namesTitle = '';
-  namesTitle += `
-      <li><a href="${item}">${item}</a></li>`;
-      <li><a href="${item.show.name}">${item.show.name}</a></li>`;
-  console.log(item.show.name);
-
-  seriesResult += `
-    <li>
-      <h2>Name: ${item.name}</h2>
-      <h2>${item.show.name}</h2>
-      <ul>
-        ${namesTitle}
-      </ul>
-    </li>
-  `;
-  console.log('prueba');
-
-}
-result.innerHTML += seriesResult;
-}); */
