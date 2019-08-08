31bedd677be00dcd4ab3c8fe10aed980e53bd9b9
Marta Prieto <martaprietoluque@gmail.com>
2019-08-08 09:17:19 +0200

    main html structure

---
+++ _src/assets/js/main.js
@@ -0,0 +1,19 @@
+'use strict';
+
+console.log('>> Ready :)');
+
+
+const title = document.querySelector ('.input__name');
+const button = document.querySelector ('.btn');
+
+const endpoint = '';
+const favs = [];
+
+
+function writeTitle () {
+  console.log ('prueba');
+}
+
+
+
+button.addEventListener('click', writeTitle);
