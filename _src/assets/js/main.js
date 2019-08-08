3dc51260b199a499697b15d059d208a2232e3535
Marta Prieto <martaprietoluque@gmail.com>
2019-08-08 12:21:10 +0200

    fix attached image

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -15,23 +15,16 @@ function getList() {
   fetch(endpoint)
     .then(response => response.json())
     .then(data => {
-      result.innerHTML = '';
       let seriesResult = '';
       for (const item of data) {
 
-        let namesTitle = '';
-        namesTitle += `
-            <li><a href="${item.show.name}">${item.show.name}</a></li>`;
-        console.log(item.show.name);
-
         seriesResult += `
           <li>
-            <h2>${item.show.name}</h2>
-            <ul>
-              ${namesTitle}
-            </ul>
+            <h2 class="title__line"> ${item.show.name}</h2>
+            <img class="img__serie" src="${item.show.image.medium}" alt="imagen de:${item.show.name}">
           </li>
         `;
+        console.log(item.show.image.medium);
 
       }
       result.innerHTML += seriesResult;
@@ -40,3 +33,26 @@ function getList() {
 
 button.addEventListener('click', getList);
 
+
+
+
+/*   let namesTitle = '';
+  namesTitle += `
+      <li><a href="${item}">${item}</a></li>`;
+      <li><a href="${item.show.name}">${item.show.name}</a></li>`;
+  console.log(item.show.name);
+
+  seriesResult += `
+    <li>
+      <h2>Name: ${item.name}</h2>
+      <h2>${item.show.name}</h2>
+      <ul>
+        ${namesTitle}
+      </ul>
+    </li>
+  `;
+  console.log('prueba');
+
+}
+result.innerHTML += seriesResult;
+}); */
